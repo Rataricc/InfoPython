282fcb12602795ba9cdbe0a86ef80568d4497252
Lautaro Rataric <lautarorataric163@gmail.com>
2022-03-03 00:25:44 -0300

    Agregue herencia de template y un archivo login - seguir viendo...

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import inicio
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',inicio)
+    path('',views.inicio),
+    path('login/',views.login, name="login")
 ]
