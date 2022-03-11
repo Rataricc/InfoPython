65dd16110bff2736840c632ce0e885ce6d5a3a48
Lautaro Rataric <lautarorataric163@gmail.com>
2022-03-11 18:51:12 -0300

    Agregue views y template de regitro todavia sin ninguna funcionalidad, modifique inicio.html

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.inicio, name='principal'),
     path('login/', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
-    path('logout/', auth_views.logout_then_login, name="logout")
+    path('logout/', auth_views.logout_then_login, name="logout"),
+    path('registro/', views.registro_usuario, name='registro')
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -4,3 +4,9 @@ def inicio(request):
 	template_name = "inicio.html"
 	ctx = {}
 	return render(request, template_name, ctx)
+
+
+def registro_usuario(request): 
+	template_name = "registro.html"
+	ctx = {}
+	return render(request, template_name, ctx)
