74c88cddb853c818532b259d435894dd50596996
Lautaro Rataric <lautarorataric163@gmail.com>
2022-03-15 04:05:51 -0300

    0001-Agregue views de intro python y archivo html del mismo.

--- src/apps/usuarios/urls.py
+++ src/apps/usuarios/urls.py
@@ -5,4 +5,5 @@ app_name = "usuarios"
 
 urlpatterns = [
 	path('registro/', views.registro_de_usuario, name="registro"),
+	path('intro/', views.introduccion_a_python, name="intro"),
 ]
--- src/apps/usuarios/views.py
+++ src/apps/usuarios/views.py
@@ -6,7 +6,7 @@ from .forms import UserForm
 
 
 def registro_de_usuario(request): 
-	template_name = "registro.html"
+	template_name = "usuarios/registro.html"
 	
 	if request.method == 'POST': 
 		form = UserForm(request.POST)
@@ -18,4 +18,9 @@ def registro_de_usuario(request):
 		'form':UserForm
 	}
 	return render(request, template_name, ctx)
-# Create your views here.
+
+@login_required
+def introduccion_a_python(request): 
+	template_name = "usuarios/introPython.html"
+	ctx = {}
+	return render(request, template_name, ctx)
