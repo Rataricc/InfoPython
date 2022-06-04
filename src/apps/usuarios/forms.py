1d5748ecb7d08e8787b430dff7c10d25c2bf6338
Lautaro Rataric <lautarorataric163@gmail.com>
2022-06-04 10:51:11 -0300

    cambie el nombre de la variable username en forms.py

--- src/apps/usuarios/forms.py
+++ src/apps/usuarios/forms.py
@@ -10,20 +10,21 @@ class UserForm(UserCreationForm):
 	email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
 	fecha_de_nacimiento = forms.DateField(widget=forms.DateInput(attrs={'class':'form-control'}))
 	localidad = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-	username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+	nombre_de_usuario = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
 	password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
 	password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
 
 	class Meta: 
 		model = Usuario
-		fields = ('nombre', 'apellido', 'username', 'email', 'fecha_de_nacimiento', 'localidad', 'password1', 'password2')
+		fields = ('nombre', 'apellido', 'nombre_de_usuario', 'email', 'fecha_de_nacimiento', 'localidad', 'password1', 'password2')
 	
-	def clean_username(self): 
-		username = self.cleaned_data["username"]
+	def clean_nombre_de_usuario(self): 
+		nombre_de_usuario = self.cleaned_data["nombre_de_usuario"]
 
-		if len(username) < 3: 
-			self.add_error("username", "La longitud del nombre de usuario debe de ser mayor a 3")
-		return username
+		if len(nombre_de_usuario) < 3: 
+			self.add_error("nombre_de_usuario", "La longitud del nombre de usuario debe de ser mayor a 3")
+
+		return nombre_de_usuario
 	"""
 	def clean_password1(self): 
 		password1 = self.cleaned_data["password1"]
